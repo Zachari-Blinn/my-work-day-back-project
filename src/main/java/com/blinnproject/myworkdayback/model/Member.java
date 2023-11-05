05001a6776498d4e1bd4b54858e751d056df37d0
zachari blinn <zacblinn@hotmail.fr>
2023-11-05 11:33:18 +0100

    feat: add repository test for member

--- src/main/java/com/blinnproject/myworkdayback/model/Member.java
+++ src/main/java/com/blinnproject/myworkdayback/model/Member.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
@@ -15,6 +16,7 @@ import java.time.Instant;
 @Entity
 @Getter
 @Setter
+@Builder
 @AllArgsConstructor
 public class Member {
   @Id
