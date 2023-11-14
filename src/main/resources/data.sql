5c173ed601b76602b70613d1650147911a6b08c5
Zachari Blinn <zachari.blinn@eiffage.com>
2023-11-14 10:27:45 +0100

    feat: init

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, username, gender, email, password) VALUES (1, 'jean-jacques.cameron', 1, 'jean-jacques.cameron@fake-email.fr', 'Toto@123*');
+INSERT INTO users (id, username, gender, email, password) VALUES (2, 'elise.baker', 2, 'elise.baker@fake-email.fr', 'Toto@123*');
+INSERT INTO users (id, username, gender, email, password) VALUES (3, 'adam.berry', 0, 'adam.berry@fake-email.fr', 'Toto@123*');
