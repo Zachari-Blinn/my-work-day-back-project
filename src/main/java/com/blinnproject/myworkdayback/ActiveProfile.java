3369a61eb486cd235790c16fc786a60765d52ec7
Zachari Blinn <zachari.blinn@eiffage.com>
2023-11-30 16:35:17 +0100

    feat: clean

--- src/main/java/com/blinnproject/myworkdayback/ActiveProfile.java
+++ src/main/java/com/blinnproject/myworkdayback/ActiveProfile.java
@@ -1,18 +1,30 @@
 package com.blinnproject.myworkdayback;
 
+import com.blinnproject.myworkdayback.seed.DataLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
+
 @Component
 public class ActiveProfile {
+
+  private static final Logger logger = LoggerFactory.getLogger(ActiveProfile.class);
 
   @Value("${spring.profiles.active}")
   private String profile;
 
   @EventListener(ContextRefreshedEvent.class)
   public void onStartUp() {
-    System.out.println("The current profile is : " +  profile);
+    logger.warn("--------------------------------------------------------------------------------------------");
+    logger.warn("The current profile is : " +  profile);
+    if (!Objects.equals(profile, "prod")) {
+      logger.warn("PLEASE RESTART WITH THE PROD PROFILE ARGS IF YOU WANT TO LAUNCH PROJECT IN PRODUCTION MODE");
+    }
+    logger.warn("--------------------------------------------------------------------------------------------");
   }
 }
